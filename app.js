7c674b4f713b5cd58ce772a4dda59f47cb69cbfc
DJ <DJ@Dariuss-MacBook-Pro.local>
2022-10-19 15:12:22 -0400

    bugs

---
+++ app.js
@@ -0,0 +1,64 @@
+var a = 2;
+var b = 3;
+
+
+function sum(n1,n2){
+    return n1 +n2;
+}
+
+console.log(sum(a, b));
+
+//arrow function below
+(a, b) => a + b
+
+const a = 2
+
+const b = 3
+//arrow function above
+
+function subtract(param1, param2){
+    return param1 - param2;
+}
+
+console.log(subtract(a, b));
+
+//arrow function below
+(a, b) => a - b
+
+const a = 2
+
+const b = 3
+//arrow funciton above
+
+
+function multiply()
+
+const newSum = (n1, n2) => {
+    return n1 * n2
+}
+
+console.log(multiply(a,b));
+
+//arrow function below
+(a, b) => a * b
+
+const a = 2
+
+const b = 3
+//arrow function above
+
+function divide()
+
+const newestSum = (n1, n2) => {
+    return n1 / n2
+}
+
+console.log(divide(a,b));
+
+//arrow function below
+(a, b) => a / b
+
+const a = 2
+
+const b = 3
+//arrow function above
